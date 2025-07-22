b6185521a8d6268aaffebe624a44640a55457032
Varun Rayen <varun@hopstack.io>
2025-07-22 08:05:00 +0530

    feat(app): add morgan for logging requests

--- src/app.ts
+++ src/app.ts
@@ -1,6 +1,7 @@
 import express from 'express';
 import cors from 'cors';
 import helmet from 'helmet';
+import morgan from 'morgan';
 import config from './config/config';
 import tenantsRouter from './routes/tenants';
 import DatabaseConnection from './config/database';
@@ -8,6 +9,7 @@ import DatabaseConnection from './config/database';
 const app = express();
 
 app.use(helmet());
+app.use(morgan(config.nodeEnv === 'production' ? 'combined' : 'dev'));
 app.use(cors());
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
